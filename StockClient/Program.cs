89a122dd27c0f01f676f1461e002d12be6bdebd5
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-09 14:00:35 +0100

    Hello!

--- StockClient/Program.cs
+++ StockClient/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace StockClient
+﻿using System;
+using System.Threading.Channels;
+namespace StockClient
 {
     public class StockClient
     {
-    
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Haykel and Daniel!");
+        }
     }
 }
