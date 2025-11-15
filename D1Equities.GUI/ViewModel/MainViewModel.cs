4f2d1c787461ace38304d8964129b26e54351639
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-15 21:54:33 +0100

    feat: Implementerade MainViewModel med navigeringslogik och ikonstöd

---
+++ D1Equities.GUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace D1Equities.GUI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Fields
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+        //properties
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+        public string Caption
+        {
+            get
+            {
+                return _caption;
+            }
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon
+        {
+            get
+            {
+                return _icon;
+            }
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+        //--> Commands
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowMarketViewCommand { get; }
+        public ICommand ShowWalletViewCommand { get; }
+
+        public MainViewModel()
+        {
+            //initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowMarketViewCommand = new ViewModelCommand(ExecuteShowMarketCommand);
+            ShowWalletViewCommand = new ViewModelCommand(ExecuteShowWalletCommand);
+
+            //default view
+            ExecuteShowHomeViewCommand(null);
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Home";
+            Icon = IconChar.Home;
+        }
+
+        private void ExecuteShowMarketCommand(object obj)
+        {
+            CurrentChildView = new MarketViewModel();
+            Caption = "Market";
+            Icon = IconChar.ChartLine;
+
+        }
+
+        private void ExecuteShowWalletCommand(object obj)
+        {
+            CurrentChildView = new WalletViewModel();
+            Caption = "Wallet";
+            Icon = IconChar.CreditCard;
+        }
+    }
+}
