0cdb509527fec98b2259bc347aca610f17c75254
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-15 15:21:47 +0100

    feat: Implementerade en LoginViewModel klass som hanterar inloggningsvyns tillstånd och logik.

---
+++ D1Equities.GUI/ViewModel/LoginViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace D1Equities.GUI.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //Fields
+
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        //Properties
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get
+            {
+                return _isViewVisible;
+            }
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+        //Commands
+        public ICommand LoginCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+
+        //constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            
+        }
+
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+ 
+            bool ValidData;
+            if(string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+            {
+                ValidData = false;
+            }
+            else
+            {
+                ValidData = true;
+            }
+            return ValidData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            IsViewVisible = false;
+        }
+        
+
+    }
+}
