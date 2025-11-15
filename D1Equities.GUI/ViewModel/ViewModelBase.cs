eccbc25ed3f75d42e63ac4bf4b9cd61b850336ab
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-15 15:16:42 +0100

    Feat: Skapade en basklass för dom olika ViewModels

---
+++ D1Equities.GUI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace D1Equities.GUI.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
