404e8102df10f3edac224c844da9d5a68903b56c
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-19 16:03:59 +0100

    Feat: Fixade ViewModelBase och implementering av PropertyChanged

--- D1Equities.GUI/ViewModel/ViewModelBase.cs
+++ D1Equities.GUI/ViewModel/ViewModelBase.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-namespace D1Equities.GUI.ViewModel
+public class ViewModelBase : INotifyPropertyChanged
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
