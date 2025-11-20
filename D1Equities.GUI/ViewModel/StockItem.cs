631c6a068e75325f48689b8c54d1d2376a3eb05d
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-20 21:59:31 +0100

    Feat: Skapade en stockitem klass som innehåller stock properties

---
+++ D1Equities.GUI/ViewModel/StockItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1Equities.GUI.ViewModel
+{
+    public class StockItem : ViewModelBase
+    {
+        public string Ticker { get; set; }
+        public double Price { get; set; }
+        public double OpenPrice { get; set; }
+        public double Percent { get; set; }
+        public double PriceDifference { get; set; }
+        public string CompanyName {  get; set; }
+    }
+}
