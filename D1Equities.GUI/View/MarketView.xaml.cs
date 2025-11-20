7c2f15ce9b858c789a606097a5cd72674f1da4f7
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-20 21:56:32 +0100

    Feat: La till asykron initiering i MarketView

--- D1Equities.GUI/View/MarketView.xaml.cs
+++ D1Equities.GUI/View/MarketView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D1Equities.GUI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,15 @@ namespace D1Equities.GUI.View
         public MarketView()
         {
             InitializeComponent();
+            Loaded += MarketView_Loaded;
+        }
+
+        private async void MarketView_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MarketViewModel vm)
+            {
+                await vm.InitializeAsync();
+            }
         }
     }
 }
