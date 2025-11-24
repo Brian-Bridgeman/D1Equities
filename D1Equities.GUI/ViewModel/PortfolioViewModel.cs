3b91d560b090e8f497963263575dc11d2d00ed97
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-24 15:33:08 +0100

    feat: Skapade en refresh metod i PortfolioviewModel

--- D1Equities.GUI/ViewModel/PortfolioViewModel.cs
+++ D1Equities.GUI/ViewModel/PortfolioViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using D1Equities.GUI.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace D1Equities.GUI.ViewModel
 {
     public class PortfolioViewModel : ViewModelBase
     {
+        public ObservableCollection<PositionItem> Positions { get; }
+        private UserModel _user;
+        public PortfolioViewModel(UserModel user)
+        {
+            _user = user ?? throw new ArgumentNullException(nameof(user));
+
+            Positions = new ObservableCollection<PositionItem>(
+                _user.Portfolio.Positions.Values.Select(p => new PositionItem(p))
+            );
+        }
+        public void Refresh()
+        {
+            // Clear and reload positions
+            Positions.Clear();
+            foreach (var p in _user.Portfolio.Positions.Values)
+                Positions.Add(new PositionItem(p));
+        }
     }
 }
