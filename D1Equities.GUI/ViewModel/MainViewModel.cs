5cf7d1e109a6cd53f5f095cd5463d1341aad1b34
Brian-Bridgeman <brian.bridgeman11@gmail.com>
2025-11-24 15:34:18 +0100

    Feat: Updaterade ExecuteBuyStock och ExecuteSell metoderna till at refresha efter varje handel av stcosk

--- D1Equities.GUI/ViewModel/MainViewModel.cs
+++ D1Equities.GUI/ViewModel/MainViewModel.cs
@@ -1,77 +1,76 @@
-﻿using D1Equities.GUI.View;
+﻿using D1Equities.GUI.Model;
+using D1Equities.GUI.View;
 using FontAwesome.Sharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace D1Equities.GUI.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        //Fields
+        // Fields
         private ViewModelBase _currentChildView;
         private string _caption;
         private IconChar _icon;
+        private readonly UserModel _currentUser;
 
-        //properties
+        // Properties
         public ViewModelBase CurrentChildView
         {
-            get
-            {
-                return _currentChildView;
-            }
+            get => _currentChildView;
             set
             {
                 _currentChildView = value;
                 OnPropertyChanged(nameof(CurrentChildView));
             }
         }
+
         public string Caption
         {
-            get
-            {
-                return _caption;
-            }
+            get => _caption;
             set
             {
                 _caption = value;
                 OnPropertyChanged(nameof(Caption));
             }
         }
+
         public IconChar Icon
         {
-            get
-            {
-                return _icon;
-            }
+            get => _icon;
             set
             {
                 _icon = value;
                 OnPropertyChanged(nameof(Icon));
             }
         }
-        //--> Commands
+
+        // Commands
         public ICommand ShowHomeViewCommand { get; }
         public ICommand ShowPortfolioViewCommand { get; }
         public ICommand ShowMarketViewCommand { get; }
         public ICommand ShowStockViewCommand { get; }
 
-        public MainViewModel()
+        // Constructor
+        public MainViewModel(UserModel user)
         {
-            //initialize commands
+            _currentUser = user ?? throw new ArgumentNullException(nameof(user));
+
             ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
             ShowPortfolioViewCommand = new ViewModelCommand(ExecuteShowPortfolioCommand);
             ShowMarketViewCommand = new ViewModelCommand(ExecuteShowMarketCommand);
             ShowStockViewCommand = new ViewModelCommand(ExecuteShowStockCommand);
 
-
-            //default view
+            // Default view
             ExecuteShowHomeViewCommand(null);
         }
 
+        // Methods for commands
         private void ExecuteShowHomeViewCommand(object obj)
         {
             App.Simulator.UnloadAllStocks();
@@ -79,10 +78,12 @@ namespace D1Equities.GUI.ViewModel
             Caption = "Home";
             Icon = IconChar.Home;
         }
+
         private void ExecuteShowPortfolioCommand(object obj)
         {
             App.Simulator.UnloadAllStocks();
-            CurrentChildView = new PortfolioViewModel();
+            // Always use the injected _currentUser
+            CurrentChildView = new PortfolioViewModel(_currentUser);
             Caption = "Portfolio";
             Icon = IconChar.CreditCard;
         }
@@ -90,11 +91,9 @@ namespace D1Equities.GUI.ViewModel
         private void ExecuteShowMarketCommand(object obj)
         {
             App.Simulator.UnloadAllStocks();
-
             CurrentChildView = new MarketViewModel();
             Caption = "Market";
             Icon = IconChar.ChartLine;
-
         }
 
         private async void ExecuteShowStockCommand(object obj)
@@ -104,12 +103,26 @@ namespace D1Equities.GUI.ViewModel
             var vm = new StockViewModel();
             await vm.InitializeAsync(ticker);
 
-
-
-            CurrentChildView = vm; // assign the view, not the VM
+            CurrentChildView = vm;
             Caption = ticker;
             Icon = IconChar.ChartLine;
+        }
+        private void ExecuteBuyStock(string ticker, decimal price, int quantity)
+        {
+            var user = (UserModel)Application.Current.Properties["User"];
+            user.Portfolio.BuyShares(ticker, price, quantity);
 
+            if (CurrentChildView is PortfolioViewModel vm)
+                vm.Refresh();
+        }
+
+        private void ExecuteSellStock(string ticker, decimal price, int quantity)
+        {
+            var user = (UserModel)Application.Current.Properties["User"];
+            user.Portfolio.SellShares(ticker, price, quantity);
+
+            if (CurrentChildView is PortfolioViewModel vm)
+                vm.Refresh();
         }
     }
 }
